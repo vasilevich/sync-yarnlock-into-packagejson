a3059801ea483f486ebf55faf89df751e3e21a6c
Jan Aagaard <jan@aagaard.net>
2022-04-23 21:36:14 +0200

    Rearrange to execute code from top to bottom

--- src/syncpackagejson.ts
+++ src/syncpackagejson.ts
@@ -4,68 +4,21 @@ import os from "os";
 import path from "path";
 import { NpmList, PackageJson, PackageVersionsAndUrls } from "./types";
 
-const getUpdatedVersionWithRange = (
-  originalVersionWithRange: string,
-  installedVersionWithoutRange: string
-) => {
-  if (originalVersionWithRange.includes("+")) {
-    return originalVersionWithRange;
-  }
-
-  if (originalVersionWithRange.includes("link:")) {
-    return originalVersionWithRange;
-  }
-
-  const range = getRange(originalVersionWithRange);
-  return range + installedVersionWithoutRange;
+const main = () => {
+  const packageJsonPath = path.resolve(process.cwd(), "package.json");
+  const installedPackages = getNpmListOutput().dependencies;
+  synchronizeInstalledVersionsIntoPackageJson(
+    packageJsonPath,
+    installedPackages
+  );
 };
 
-const getRange = (versionWithRange: string): string => {
-  // Match any combination of ^, <, >, = and ~ characters at the beginning of the string.
-  const rangeMatches = versionWithRange.match(/(^[\^><=~]+)/);
-  if (rangeMatches === null) {
-    return "";
-  }
-
-  return rangeMatches[0];
-};
-
-const syncIntoPackageJson = (
-  packageJsonObject: PackageJson,
-  installedPackages: PackageVersionsAndUrls
-) => {
-  const dependencyNames = Object.keys(installedPackages);
-  dependencyNames.forEach((dependencyName) => {
-    const name = dependencyName;
-    const version = installedPackages[dependencyName].version;
-
-    if (
-      packageJsonObject.dependencies &&
-      name in packageJsonObject.dependencies
-    ) {
-      packageJsonObject.dependencies[name] = getUpdatedVersionWithRange(
-        packageJsonObject.dependencies[name],
-        version
-      );
-    } else if (
-      packageJsonObject.devDependencies &&
-      name in packageJsonObject.devDependencies
-    ) {
-      packageJsonObject.devDependencies[name] = getUpdatedVersionWithRange(
-        packageJsonObject.devDependencies[name],
-        version
-      );
-    }
-  });
-};
-
-const getEolCharacter = (source: string) => {
-  const match = source.match(/\r?\n/);
-  return match === null ? os.EOL : match[0];
+const getNpmListOutput = (): NpmList => {
+  return JSON.parse(childProcess.execSync("npm list --json").toString());
 };
 
 // Only the root package.json file contains a workspaces field but to simplify the code we don't separate the logic.
-const updatePackageJson = (
+const synchronizeInstalledVersionsIntoPackageJson = (
   packageJsonPath: string,
   rootDeps: PackageVersionsAndUrls
 ) => {
@@ -76,7 +29,7 @@ const updatePackageJson = (
   const originalPackageJsonText = fs.readFileSync(packageJsonPath, "utf8");
   const packageJson = JSON.parse(originalPackageJsonText) as PackageJson;
 
-  syncIntoPackageJson(packageJson, rootDeps);
+  updatePackageJsonObject(packageJson, rootDeps);
   const updatedPackageJsonText = (
     JSON.stringify(packageJson, null, 2) + "\n"
   ).replace(/\r?\n/g, getEolCharacter(originalPackageJsonText));
@@ -119,10 +72,64 @@ const updatePackageJson = (
   // }
 };
 
-const getNpmListOutput = (): NpmList => {
-  return JSON.parse(childProcess.execSync("npm list --json").toString());
+const updatePackageJsonObject = (
+  packageJson: PackageJson,
+  installedPackages: PackageVersionsAndUrls
+) => {
+  const dependencyNames = Object.keys(installedPackages);
+  dependencyNames.forEach((dependencyName) => {
+    const installedVersionWithoutRange =
+      installedPackages[dependencyName].version;
+
+    if (
+      packageJson.dependencies &&
+      dependencyName in packageJson.dependencies
+    ) {
+      packageJson.dependencies[dependencyName] = getUpdatedVersionWithRange(
+        packageJson.dependencies[dependencyName],
+        installedVersionWithoutRange
+      );
+    } else if (
+      packageJson.devDependencies &&
+      dependencyName in packageJson.devDependencies
+    ) {
+      packageJson.devDependencies[dependencyName] = getUpdatedVersionWithRange(
+        packageJson.devDependencies[dependencyName],
+        installedVersionWithoutRange
+      );
+    }
+  });
 };
 
-const packageJsonPath = path.resolve(process.cwd(), "package.json");
-const installedPackages = getNpmListOutput().dependencies;
-updatePackageJson(packageJsonPath, installedPackages);
+const getUpdatedVersionWithRange = (
+  originalVersionWithRange: string,
+  installedVersionWithoutRange: string
+) => {
+  if (originalVersionWithRange.includes("+")) {
+    return originalVersionWithRange;
+  }
+
+  if (originalVersionWithRange.includes("link:")) {
+    return originalVersionWithRange;
+  }
+
+  const range = getRange(originalVersionWithRange);
+  return range + installedVersionWithoutRange;
+};
+
+const getRange = (versionWithRange: string): string => {
+  // Match any combination of ^, <, >, = and ~ characters at the beginning of the string.
+  const rangeMatches = versionWithRange.match(/(^[\^><=~]+)/);
+  if (rangeMatches === null) {
+    return "";
+  }
+
+  return rangeMatches[0];
+};
+
+const getEolCharacter = (source: string) => {
+  const match = source.match(/\r?\n/);
+  return match === null ? os.EOL : match[0];
+};
+
+main();
